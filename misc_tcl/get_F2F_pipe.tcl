9cba0fcafc9403c33160de5ed109a6139386968a
Chandler zhao <zhao_chunxiao@hotmail.com>
2019-07-31 09:25:11 +0800

    Update get_F2F_pipe.tcl

--- misc_tcl/get_F2F_pipe.tcl
+++ misc_tcl/get_F2F_pipe.tcl
@@ -9,3 +9,10 @@ foreach din [get_pins -filter “REF_PIN_NAME==D && SETUP_SLACK>1.0” -of [get_
 report_timing -to $pipes -max 1000 -input_pins -name pipes
 
 #Make sure to look at the slack before and after the pipeline stage, which report_design_analysis can help you do.
+
+##########
+#Alternative approach this will take much longer time because it will report timing first.
+#comment this off, this is just an idea, and workable for small design.
+if { 0 }  {
+    report_timing -name healthy_FF2FF -of_objects [get_timing_paths -filter {LOGIC_LEVELS==0 && SLACK > 1} -max_paths 100000 -setup]
+}
